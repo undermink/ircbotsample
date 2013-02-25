2a138502e677a89dffcac04885b5a83122d80288
undermink <undermink@devtal.de>
2013-02-25 01:00:38 +0100

    nicer email formatting

--- email.rb
+++ email.rb
@@ -1,39 +1,50 @@
 #!/usr/bin/env ruby
 require 'mail'
-require 'pony'
 
 def tellundermink(who)
   pp 'sending message to undermink'
-  Pony.mail(:to => 'undermink@chaostal.de', :via => :sendmail, :body => 'powerbot hier\nkomm mal in den irc.\n#{who}')
-  #mail = Mail.new do
-  #  from    'powerbot@sunnata.de'
-  #  to      'undermink@chaostal.de'
-  #  subject 'IRC'
-  #  body    'Hi\nHier ist powerbot...\nDu sollst bitte mal ins irc kommen.\nSagt #{who}'
-  #end
-  #mail.delivery_method :sendmail
-  #mail.deliver
+  mail = Mail.new do
+    from    'powerbot@sunnata.de'
+    to      'undermink@chaostal.de'
+    subject 'IRC'
+    body    'Hi undermink
+    Hier ist powerbot...
+    Du sollst bitte mal in den irc kommen.
+    Soll ich Dir von '+who+' sagen...'
+  end
+  mail.delivery_method :sendmail
+  mail.deliver
   pp '...done'
 end
 
 def tellnora(who)
   pp 'sending message to nora'
-  Mail.deliver do
+  mail = Mail.new do
     from    'powerbot@sunnata.de'
     to      'nora@sunnata.de'
     subject 'IRC'
-    body    'Hi\nHier ist powerbot...\nDu sollst bitte mal ins irc kommen.\nSagt #{who}'
+    body    'Hi Nora
+    Hier ist powerbot...
+    Du sollst bitte mal ins irc kommen.
+    Soll ich Dir von '+who+' sagen.'
   end
+  mail.delivery_method :sendmail
+  mail.deliver
   pp '...done'
 end
 
 def tellmettfabrik(who)
   pp 'sending message to mettfabrik'
-  Mail.deliver do
+  mail = Mail.new do
     from    'powerbot@sunnata.de'
     to      'mettfabrik@sunnata.de'
     subject 'IRC'
-    body    'Hi\nHier ist powerbot...\nDu sollst bitte mal ins irc kommen.\nSagt #{who}'
+    body    'Hi Simon
+    Hier ist powerbot...
+    Du sollst bitte mal ins irc kommen.
+    Soll ich Dir von '+who+' sagen.'
   end
+  mail.delivery_method :sendmail
+  mail.deliver
   pp '...done'
 end
