d6eb850bba0a1dce1db7d86ff39016e0b010464d
undermink <undermink@devtal.de>
2013-03-01 01:29:43 +0100

    join #snow and draw a snowman

---
+++ snowman.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+def snowman
+  man="\n
+,    ______  ,    ,\n
+  ,  |    |    ,\n
+    _|____|_  ,  ,,\n
+ ,   ( o o)| | |    ,\n
+    (   |  )\\|/  ,\n
+ , (    .   )|)    ,\n
+    (   .   )|  ,\n
+ ___(_______)|______\n
+\n"
+puts man
+return man.split("\n")
+end
